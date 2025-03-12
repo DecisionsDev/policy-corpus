407ebeae448f05d4d5cf34b04063b971c6cfa87d
mariia.berdnyk <mariia.berdnyk@ibm.com>
2025-03-12 17:21:35 +0100

    watsonx implementation

--- common/llm_calls.py
+++ common/llm_calls.py
@@ -4,7 +4,7 @@ import sys
 import time
 from enum import Enum
 from pathlib import Path
-from typing import Tuple, Dict
+from typing import Tuple, Dict, List
 import json
 
 import pandas as pd
@@ -49,42 +49,49 @@ def extract_json_from_response(response):
     return None if not code_string else code_string
 
 
-def call_ollama(model_config: Dict, system_prompt, user_prompt) -> Tuple[str, str, int]:
+def call_ollama(model_config: Dict, system_prompt, user_prompts) -> Tuple[List[str | None], List[str | None], List[int]]:
     try:
         import ollama
     except ImportError as e:
         raise e
 
-    messages = [{"role": "system", "content": system_prompt}, {"role": "user", "content": user_prompt}]
-    generated_code = None
-    generated_response = None
-    total_completion_token = 0
+    messages = [{"role": "system", "content": system_prompt}]
+    generated_codes = []
+    generated_responses = []
+    completion_tokens = []
 
     options = model_config["options"]
-    options["additional_num_ctx"] += len(system_prompt) + len(user_prompt)
+    options["additional_num_ctx"] += len(system_prompt) + len(max(user_prompts, key=len))
+    for user_prompt in user_prompts:
+        if len(messages) == 1:
+            messages.append({"role": "user", "content": user_prompt})
+        else:
+            messages[1] = {"role": "user", "content": user_prompt}
 
-    for attempt in range(1000):
-        try:
-            response_cur = ollama.chat(
-                model=model_config["model_name"], messages=messages, stream=False,
-                options=options
-            )
-            if not response_cur["done"]:
-                raise Exception("Non-200 response: " + str(response_cur))
-            generated_response = response_cur["message"]["content"]
-            generated_code = extract_json_from_response(generated_response)
+        for attempt in range(1000):
+            try:
+                response_cur = ollama.chat(
+                    model=model_config["model_name"], messages=messages, stream=False,
+                    options=options
+                )
+                if not response_cur["done"]:
+                    raise Exception("Non-200 response: " + str(response_cur))
+                generated_response = response_cur["message"]["content"]
+                generated_code = extract_json_from_response(generated_response)
 
-            if generated_code:
-                total_completion_token += response_cur["eval_count"]
-                break
-        except Exception as e:
-            print(f"Attempt {attempt + 1} failed with error: {e}")
-            time.sleep(1)
+                if generated_code:
+                    completion_tokens.append(response_cur["eval_count"])
+                    generated_codes.append(generated_code)
+                    generated_responses.append(generated_response)
+                    break
+            except Exception as e:
+                print(f"Attempt {attempt + 1} failed with error: {e}")
+                time.sleep(1)
 
-    return generated_response, generated_code, total_completion_token
+    return generated_responses, generated_codes, completion_tokens
 
 
-def call_watsonxai(model_config: Dict, system_prompt, user_prompt) -> Tuple[str, str, int]:
+def call_watsonxai(model_config: Dict, system_prompt, user_prompts: []) -> Tuple[List[str | None], List[str | None], List[int]]:
     try:
         from langchain_core.messages import SystemMessage, HumanMessage
         from langchain_ibm import ChatWatsonx
@@ -93,7 +100,7 @@ def call_watsonxai(model_config: Dict, system_prompt, user_prompt) -> Tuple[str,
 
     os.environ["WATSONX_APIKEY"] = os.getenv("IBM_API_KEY")
 
-    messages = [SystemMessage(content=system_prompt), HumanMessage(content=user_prompt)]
+    messages = [SystemMessage(content=system_prompt)]
 
     chat = ChatWatsonx(
         model_id=model_config["model_id"],
@@ -102,44 +109,42 @@ def call_watsonxai(model_config: Dict, system_prompt, user_prompt) -> Tuple[str,
         params=model_config["options"] if model_config["options"] else DEFAULT_PARAMETERS,
     )
 
-    generated_code = None
-    generated_response = None
-    total_completion_token = 0
+    generated_codes = []
+    generated_responses = []
+    number_tokens = []
 
-    for attempt in range(1000):
-        try:
-            response_cur = chat.invoke(messages)
+    for user_prompt in user_prompts:
+        if len(messages) == 1:
+            messages.append(user_prompt)
+        else:
+            messages[1] = user_prompt
 
-            generated_response = response_cur.content
-            generated_code = extract_json_from_response(generated_response)
+        for attempt in range(1000):
+            try:
+                response_cur = chat.invoke(messages)
 
-            if generated_code:
-                total_completion_token += response_cur.response_metadata['token_usage']['generated_token_count'] + \
-                        response_cur.response_metadata['token_usage']['input_token_count']
-                break
-        except Exception as e:
-            print(f"Attempt {attempt + 1} failed with error: {e}")
-            time.sleep(1)
+                generated_response = response_cur.content
+                generated_code = extract_json_from_response(generated_response)
 
-    return generated_response, generated_code, total_completion_token
+                if generated_code:
+                    number_tokens.append(response_cur.response_metadata['token_usage']['completion_tokens'])
+                    generated_codes.append(generated_code)
+                    generated_responses.append(generated_response)
+                    break
+            except Exception as e:
+                print(f"Attempt {attempt + 1} failed with error: {e}")
+                time.sleep(1)
+
+    return generated_responses, generated_codes, number_tokens
 
 
-def call_api(llm_api, model_config, system_prompt, user_prompt) -> Tuple[str, str, int]:
+def call_api(llm_api, model_config, system_prompt, user_prompts) -> Tuple[List[str], List[str], List[int]]:
     if llm_api == LLM_API.OLLAMA:
-        return call_ollama(model_config, system_prompt, user_prompt)
+        return call_ollama(model_config, system_prompt, user_prompts)
     elif llm_api == LLM_API.WATSONXAI:
-        return call_watsonxai(model_config, system_prompt, user_prompt)
+        return call_watsonxai(model_config, system_prompt, user_prompts)
 
-    return "No supported LLM API type provided", "", 0
-
-
-def call_api(llm_api, model_config, system_prompt, user_prompt) -> Tuple[str, str, int]:
-    if llm_api == LLM_API.OLLAMA:
-        return call_ollama(model_config, system_prompt, user_prompt)
-    elif llm_api == LLM_API.WATSONXAI:
-        return call_watsonxai(model_config, system_prompt, user_prompt)
-
-    return "No supported LLM API type provided", "", 0
+    return ["No supported LLM API type provided"], [""], [0]
 
 
 def call_llm(policy_description_file_path, csv_file, data_generator_class: DataGenerator, llm_api: LLM_API, config_file_path, result_output_path):
@@ -156,17 +161,20 @@ def call_llm(policy_description_file_path, csv_file, data_generator_class: DataG
     dataFull = dataFull.sample(frac=1).reset_index(drop=True)
 
     data = dataFull.drop(columns=data_generator_class.EVAL_COLUMN_NAMES)
-    # data = data.head(10)
+    data = data.head(5)
 
     results = {}
+    user_prompts = []
     for idx, case in data.iterrows():
-        user_prompt = user_prompt_default.format(test_case=case.to_json())
-        generated_response, generated_answer, number_tokens = call_api(llm_api, model_config, system_prompt, user_prompt)
+        user_prompts.append(user_prompt_default.format(test_case=case.to_json()))
+
+    generated_responses, generated_answers, numbers_tokens = call_api(llm_api, model_config, system_prompt, user_prompts)
+    for idx in range(len(generated_answers)):
         results[idx] = {
             "test_case": dataFull.iloc[[idx]].to_dict(),
-            "generated_response": generated_response,
-            "generated_answer": json.loads(generated_answer) if generated_answer else None,
-            "number_tokens": number_tokens
+            "generated_response": generated_responses[idx],
+            "generated_answer": json.loads(generated_answers[idx]) if generated_answers[idx] else None,
+            "number_tokens": numbers_tokens[idx]
         }
 
     results_output_path = Path(result_output_path)
@@ -184,5 +192,13 @@ if __name__ == "__main__":
         LuggageDataGenerator(),
         LLM_API.OLLAMA,
         "./config/ollama_config_example.json",
-        "generation_result.json",
+        "generation_result_watson.json",
     )
+    # call_llm(
+    #     "../luggage/luggage_policy.txt",
+    #     "../luggage/luggage_compliance/luggage_policy_test_dataset_100.csv",
+    #     LuggageDataGenerator(),
+    #     LLM_API.WATSONXAI,
+    #     "./config/watsonx_config_example.json",
+    #     "generation_result_watson.json",
+    # )
